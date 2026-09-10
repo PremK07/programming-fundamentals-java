b95b680f309551a48e8dff6bc22fe26ed99eedf1
PremK07 <160155567+PremK07@users.noreply.github.com>
2026-09-10 14:56:45 +0530

    ✅ Add solution for: Using Compound Assignment Operators

---
+++ exercises/UsingCompoundAssignmentOperators.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+public class Solution { 
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in); 
+        
+        // Write your code here to apply the compound assignment 
+        //operators 
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(a+b-c);
+
+        
+        sc.close(); 
+    } 
+}
