a0e1945bd36346e6958db31d59a773787bc0f160
PremK07 <160155567+PremK07@users.noreply.github.com>
2026-09-10 15:00:20 +0530

    ✅ Add solution for: Are the Numbers Equal?

---
+++ exercises/AreTheNumbersEqual.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+public class Solution { 
+    public static void main(String[] args) { 
+        // Take help of Scanner to take input 
+        Scanner sc = new Scanner(System.in); 
+        int num1 = sc.nextInt(); 
+        int num2 = sc.nextInt(); 
+        System.out.println(num1==num2);
+        
+        // Use logical operator
+        // print the result according to the question
+
+        sc.close(); 
+    } 
+}
