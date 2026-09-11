1f2d6ecc75fa41365eae4eb255a096e9acd1d9ce
PremK07 <160155567+PremK07@users.noreply.github.com>
2026-09-11 11:18:40 +0530

    ✅ Add solution for: Farmer’s Total earnings

---
+++ exercises/FarmerSTotalEarnings.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+public class Solution { 
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in);
+        double pricePerKg = scanner.nextDouble(); 
+        int quantitySold = scanner.nextInt(); 
+        
+        // Write your code here
+        double totalearnings = pricePerKg*quantitySold;
+        System.out.printf("%.2f",totalearnings);
+       
+
+        scanner.close(); 
+    } 
+}
